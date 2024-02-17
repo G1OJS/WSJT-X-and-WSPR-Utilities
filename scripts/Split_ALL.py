fc1f2867d61371eaa4234f7f2c528ed4064aa989
G1OJS <140509227+G1OJS@users.noreply.github.com>
2024-02-17 10:53:16 +0000

    Update Split_ALL.py

--- scripts/Split_ALL.py
+++ scripts/Split_ALL.py
@@ -1,1 +1,34 @@
+# This script opens ALL.TXT and copies entries into files named ALL_YYMM.txt in the same directory
+#
+# TO RUN:
+# Make sure Python is installed
+# Copy or download this script, edit path to point to where ALL.TXT is stored, ans save the script as "Split_ALL.py"
+# Open a command window, change directory to wherever you've saved this script, and type Python "Split_ALL.py"
+#
+# USE AT YOUR OWN RISK & BACK UP important files FIRST
 
+
+
+
+
+
+
+import os
+
+path=r"C:\Users\drala\AppData\Local\WSJT-X"
+
+fi = open(path + "\\ALL.TXT" , "r")
+fo_me = open(path + "\\ALL_me.TXT" , "a")
+
+mth=""
+for line in fi:
+	if("G1OJS" in line): fo_me.write(line)
+
+	if(line[0:4]!=mth):
+		mth=line[0:4]
+		if mth.isnumeric(): fo = open(path + "\\ALL_" + mth + ".txt" , "w")
+	else:
+		fo.write(line)
+
+fi.close()
+		
