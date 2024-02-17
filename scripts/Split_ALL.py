a48d77b4f6ac296e956630d3df8f6450216f99a7
G1OJS <140509227+G1OJS@users.noreply.github.com>
2024-02-17 14:35:52 +0000

    Update Split_ALL.py

--- scripts/Split_ALL.py
+++ scripts/Split_ALL.py
@@ -1,7 +1,7 @@
 # This script opens ALL.TXT and copies entries into files named ALL_YYMM.txt in the same directory
 # Additionally, if you edit myCall below, the script will create ALL_me.txt with only entries containing your callsign.
 # It is left to the user to decide how to manage the resulting files.
-# You may wish to delete the existing ALL.TXT after the script has run, and replace it by renaming the latest ALL_YYMM.txt file to ALL.TXT
+# You may wish to delete (or archive) the existing ALL.TXT after the script has run, and replace it by renaming the latest ALL_YYMM.txt file to ALL.TXT
 # This way you won't create duplicate entries when you run the script again
 # If you leave the ALL_me.txt file where it is, it will be appended next time you run the script
 # Tip - don't do this whilst WSJT-X is running as it will recreate the ALL.TXT file as soon as you delete it
@@ -27,6 +27,10 @@ for line in fi:
 
 	if(line[0:4]!=mth):
 		mth=line[0:4]
+		try: 
+			fo.close() 
+		except Exception: 
+			pass
 		if mth.isnumeric(): fo = open(path + "\\ALL_" + mth + ".txt" , "w")
 	else:
 		fo.write(line)
